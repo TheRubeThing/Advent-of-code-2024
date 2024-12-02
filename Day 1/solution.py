0c4adc1b94a197d504c69adf90d530e970f71954
Ruben Svensson <ruben.svensson@xlnaudio.com>
2024-12-02 10:46:55 +0100

    Day 1 added simplifed version and timeing of functions

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -1,4 +1,7 @@
-def main():
+import time
+
+# My first implementation of the problem
+def first():
     list1 = []
     list2 = []
 
@@ -36,6 +39,45 @@ def main():
                 occurance_cache[list1[i]] = count
 
         print(f"Part2: Occurance is {ocurrances}")
+
+# Wanted to see the performance without caching and storing the loop
+# iterations
+def simplified():
+    list1 = []
+    list2 = []
+
+    with open("Day 1/input.txt") as file:
+        for line in file:
+            a, b = line.split()
+            list1.append(int(a))
+            list2.append(int(b))
+        list1.sort()
+        list2.sort()
+        distance = 0
+        for item in zip(list1, list2):
+            distance += abs(item[0] - item[1])
+        print(f"Part1: Distance is {distance}")
+
+        ocurrances = 0
+        for i in range(len(list1)):
+            count = 0
+            for j in range(len(list2)):
+                if list1[i] == list2[j]:
+                    count += 1
+
+            ocurrances += list1[i] * count
+
+        print(f"Part2: Occurance is {ocurrances}")
+
+def time_function(func):
+    start = time.time()
+    func()
+    end = time.time()
+    print(f"Func {func} took {end - start} time")
+
+def main():
+    time_function(first)
+    time_function(simplified)
 
 if __name__ == "__main__":
     main()
