1113954ed355c907f5bd67f40e4cb921a7214c5a
Ruben Svensson <ruben.svensson@xlnaudio.com>
2024-12-02 10:40:50 +0100

    Day 1 part 2

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -10,10 +10,32 @@ def main():
         list1.sort()
         list2.sort()
         distance = 0
-
         for item in zip(list1, list2):
             distance += abs(item[0] - item[1])
         print(f"Part1: Distance is {distance}")
+
+        ocurrances = 0
+        occurance_cache = {}
+        previous_list2_index = 0
+
+        for i in range(len(list1)):
+            if list1[i] in occurance_cache:
+                ocurrances += occurance_cache[list1[i]] * list1[i]
+            else:
+                count = 0
+                for j in range(previous_list2_index, len(list2)):
+                    if list1[i] == list2[j]:
+                        count += 1
+                    elif list1[i] > list2[j]:
+                        continue
+                    else:
+                        previous_list2_index = j
+                        break
+
+                ocurrances += list1[i] * count
+                occurance_cache[list1[i]] = count
+
+        print(f"Part2: Occurance is {ocurrances}")
 
 if __name__ == "__main__":
     main()
